b2c72762687dbed08b85965286d9115bf774454a
Julien Fleury <you@example.comjulienfleury93@gmail.com>
2022-03-04 18:57:28 +0100

    Grabbed the firestore information. Will need to cycle through it now.

--- src/components/home/home.component.jsx
+++ src/components/home/home.component.jsx
@@ -1,12 +1,48 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './home.styles.scss';
 
-const Home = () => {
-    return(
-        <div className="homeContainer">
-            <h1>Home!</h1>
-        </div>
-    );
+import { firestore } from '../../firebase/firebase.utils';
+
+class Home extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            homePageData: {}
+        }
+    }
+
+    componentDidMount() {
+        // Grabbing Collection TextChanges
+        const textChangesRef = firestore.collection('TextChanges');
+        textChangesRef.onSnapshot(async snapshot => {
+            const docArray = snapshot.docs;
+
+            // Homepage
+            const homePageInfo = docArray[0].data();
+            this.setState({
+                homePageData: homePageInfo
+            });
+        });
+    }
+    render() {
+        const { homePageData } = this.state;
+        return (
+            <div className="homeContainer container-fluid">
+                <div className="row">
+                    <div className="col-12">
+                        <h1 className="text-center">Update the Website</h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-12 col-md-6">
+                        <div className="infoBox">
+                            <h4>{homePageData.HeroBody}</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default Home;
