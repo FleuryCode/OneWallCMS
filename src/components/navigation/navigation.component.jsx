b35904ed07912673c2e3d8a6d4a8addb36f35cf9
Julien Fleury <julienfleury93@gmail.com>
2022-03-04 10:00:52 +0100

    Changed styling of the navigation with dynamically changing svg fills based on pathname.

--- src/components/navigation/navigation.component.jsx
+++ src/components/navigation/navigation.component.jsx
@@ -1,21 +1,22 @@
 import React from "react";
 import './navigation.styles.scss';
-import { Link } from 'react-router-dom';
-import HomeIcon from '../../assets/homeIcon.svg';
-import StatsIcon from '../../assets/statsIcon.svg';
-import CalendarIcon from '../../assets/calendarIcon.svg';
+import { Link, useLocation } from 'react-router-dom';
+import { ReactComponent as HomeIcon } from '../../assets/homeIcon.svg';
+import {ReactComponent as StatsIcon} from '../../assets/statsIcon.svg';
+import {ReactComponent as CalendarIcon} from '../../assets/calendarIcon.svg';
 
 const Navigation = () => {
+    let location = useLocation();
     return (
         <div className="navigationContainer">
             <Link to={'/'} className="navItem mt-4">
-                <img src={HomeIcon} alt="Home icon" />
+                <HomeIcon className="icons" fill={`${(location.pathname === '/') ? '#99EFA0' : '#d9d9d9'}`}/>
             </Link>
-            <Link to={'/'} className="navItem">
-                <img src={StatsIcon} alt="Stats icon" />
+            <Link to={'/stats'} className="navItem">
+                <StatsIcon className="icons" fill={`${(location.pathname === '/stats') ? '#99EFA0' : '#d9d9d9'}`} />
             </Link>
-            <Link to={'/'} className="navItem">
-                <img src={CalendarIcon} alt="Calendar icon" />
+            <Link to={'/schedule'} className="navItem">
+                <CalendarIcon className="icons" fill={`${(location.pathname === '/schedule') ? '#99EFA0' : '#d9d9d9'}`} />
             </Link>
         </div>
     );
